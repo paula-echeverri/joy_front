26c02ac961c625fe3cad837694aa0d4ffa8778e3
paula-echeverri <paeb08@gmail.com>
2024-09-11 11:39:07 -0500

    feat/customize_rooms

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import "./Rooms.css";
 
-import joy from '../assets/img/joyApp.png'
+import joy from "../assets/img/joyApp.png";
 const Rooms = () => {
   const goToRoom = (event) => {
     const content = event.currentTarget.getAttribute("data-value");
     console.log("content", content);
     //create functions to call api a pass this parameters
   };
+
+  const chatRooms = [
+    {
+      value: "dogs",
+      title: "Dogs",
+      description: "Aca puedes preguntar por vacunacion, veterinaria de ...",
+    },
+    {
+      value: "cats",
+      title: "Cats",
+      description: "Aca puedes preguntar por vacunacion, veterinaria de ...",
+    },
+    {
+      value: "cart",
+      title: "Cart",
+      description:
+        "Si deseas comprar algo para tu peludito aca puedes avanzar tu compra...",
+    },
+    {
+      value: "appoiment",
+      title: "Appoiment",
+      description:
+        "Si deseas agendar una cita para que atiendan a tu peludito...",
+    },
+  ];
+
   return (
     <div>
       <div>
@@ -15,50 +41,72 @@ const Rooms = () => {
           <h1>CHAT WITH THE CHAT (ROOMS)</h1>
         </div>
         <div className="image-container">
-          <img src={joy}     style={{ width: '400px', height: '350px' }}  alt="Joy_pics" />
+          <img
+            src={joy}
+            style={{ width: "200px", height: "150px" }}
+            alt="Joy_pics"
+          />
         </div>
       </div>
 
-      <div class="chats">
-        <div class="box box1">
-          <div class="feature_image" onClick={goToRoom} data-value="dogs">
+      {/* <div className="chats">
+        <div className="box box1">
+          <div className="feature_image" onClick={goToRoom} data-value="dogs">
             <div>Dogs</div>
           </div>
-          <div class="feature-info">
+          <div className="feature-info">
             <div>Aca puedes preguntar por vacunacion , veterinaria de ...</div>
           </div>
         </div>
-        <div class="box box2">
-          <div class="feature_image" onClick={goToRoom} data-value="cats">
+        <div className="box box2">
+          <div className="feature_image" onClick={goToRoom} data-value="cats">
             <div>Cats</div>
           </div>
           <div>
-            <div class="feature-info">
+            <div className="feature-info">
               Aca puedes preguntar por vacunacion , veterinaria de ...
             </div>
           </div>
         </div>
-        <div class="box box3">
-          <div class="feature_image" onClick={goToRoom} data-value="cart">
+        <div className="box box3">
+          <div className="feature_image" onClick={goToRoom} data-value="cart">
             <div>Cart</div>
           </div>
           <div>
-            <div class="feature-info">
+            <div className="feature-info">
               Si deseas comprar algo para tu peludito aca puedes avanzar tu
               compra...
             </div>
           </div>
         </div>
-        <div class="box box4">
-          <div class="feature_image" onClick={goToRoom} data-value="appoiment">
+        <div className="box box4">
+          <div className="feature_image" onClick={goToRoom} data-value="appoiment">
             <div>Appoiment</div>
           </div>
           <div>
-            <div class="feature-info">
+            <div className="feature-info">
               Si deseas agendar una cita para que atiendan a tu peludtio...
             </div>
           </div>
         </div>
+      </div> */}
+      {/*alternative of code*/}
+
+      <div className="chats">
+        {chatRooms.map((room, index) => (
+          <div className={`box box${index + 1}`} key={room.value}>
+            <div
+              className="feature_image"
+              onClick={goToRoom}
+              data-value={room.value}
+            >
+              <div>{room.title}</div>
+            </div>
+            <div className="feature-info">
+              <div>{room.description}</div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
